ec441a2c826f64095255cd0d51726b895ebf0d01
Greg Anderson <greg@getpantheon.com>
2019-05-31 15:53:46 -0700

    Fix up autoload-dev & c. (#2)

--- tests/src/Fixtures.php
+++ tests/src/Fixtures.php
@@ -5,6 +5,7 @@ namespace SiteAudit;
 use Symfony\Component\Filesystem\Filesystem;
 
 use Drush\TestTraits\DrushTestTrait;
+use Webmozart\PathUtil\Path;
 
 /**
  * Convenience class for creating fixtures.
@@ -32,15 +33,59 @@ class Fixtures
       }
       // @todo: pull db credentials from phpunit.xml configuration
 
+      $this->createRequiredFiles();
+
       // Make settings.php writable again
       chmod('sut/web/sites/default/', 0755);
       @unlink('sut/web/sites/default/settings.php');
       copy('sut/web/sites/default/default.settings.php', 'sut/web/sites/default/settings.php');
 
       // Run site-install (Drupal makes settings.php unwritable)
-      $this->drush('site-install', [], ['db-url' => 'mysql://root@127.0.0.1/temporarysiteaudittooldatabase']);
+      $this->drush('site-install', [], ['db-url' => 'mysql://root@127.0.0.1/testsiteaudittooldatabase']);
 
       $this->installed = true;
+  }
+
+  protected function createRequiredFiles()
+  {
+    $fs = new Filesystem();
+    $drupalRoot = 'sut/web';
+
+    $dirs = [
+      'modules',
+      'profiles',
+      'themes',
+    ];
+
+    // Required for unit testing
+    foreach ($dirs as $dir) {
+      if (!$fs->exists($drupalRoot . '/'. $dir)) {
+        $fs->mkdir($drupalRoot . '/'. $dir);
+        $fs->touch($drupalRoot . '/'. $dir . '/.gitkeep');
+      }
+    }
+
+    // Prepare the settings file for installation
+    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
+      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
+      require_once $drupalRoot . '/core/includes/bootstrap.inc';
+      require_once $drupalRoot . '/core/includes/install.inc';
+      $settings['config_directories'] = [
+        CONFIG_SYNC_DIRECTORY => (object) [
+          'value' => Path::makeRelative('config/sync', $drupalRoot),
+          'required' => TRUE,
+        ],
+      ];
+      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
+    }
+
+    // Create the files directory with chmod 0777
+    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
+      $oldmask = umask(0);
+      $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
+      umask($oldmask);
+    }
   }
 
   /**
